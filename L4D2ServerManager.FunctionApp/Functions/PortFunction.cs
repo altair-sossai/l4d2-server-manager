d07dbbb6de8fa960982352921dc6cf2cbbde59f4
Altair Sossai <altair_sossai@outlook.com>
2022-08-15 10:32:14 -0300

    Endpoint to get available ports

---
+++ L4D2ServerManager.FunctionApp/Functions/PortFunction.cs
@@ -0,0 +1,30 @@
+using L4D2ServerManager.FunctionApp.Extensions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace L4D2ServerManager.FunctionApp.Functions;
+
+public class PortFunction
+{
+    private readonly IConfiguration _configuration;
+
+    public PortFunction(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    private string AuthorizationKey => _configuration.GetValue<string>(nameof(AuthorizationKey));
+
+    [FunctionName(nameof(PortFunction) + "_" + nameof(Get))]
+    public IActionResult Get([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "ports")] HttpRequest httpRequest)
+    {
+        httpRequest.EnsureAuthentication(AuthorizationKey);
+
+        var ports = new[] {27016, 27017};
+
+        return new OkObjectResult(ports);
+    }
+}
