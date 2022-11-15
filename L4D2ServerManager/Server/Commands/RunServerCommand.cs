a72ed6c014f05882b333ed1c78ca9bd17eb45a7b
Altair Sossai <altairsossai@gmail.com>
2022-11-15 09:40:04 -0300

    change +map param

--- L4D2ServerManager/Server/Commands/RunServerCommand.cs
+++ L4D2ServerManager/Server/Commands/RunServerCommand.cs
@@ -6,6 +6,6 @@ public class RunServerCommand : RunScriptCommand
 {
     public RunServerCommand(int port)
     {
-        Script.Add($"sudo screen -d -m -S \"{port}\" /home/steam/l4d2/srcds_run -port {port} -tickrate 100 -secure +mp_gamemode versus");
+        Script.Add($"sudo screen -d -m -S \"{port}\" /home/steam/l4d2/srcds_run -port {port} -tickrate 100 -secure +mp_gamemode versus +map c8m1_apartment");
     }
 }
--- L4D2ServerManager/Server/Commands/RunZoneServerCommand.cs
+++ L4D2ServerManager/Server/Commands/RunZoneServerCommand.cs
@@ -6,6 +6,6 @@ public class RunZoneServerCommand : RunScriptCommand
 {
     public RunZoneServerCommand(int port)
     {
-        Script.Add($"sudo screen -d -m -S \"{port}\" /home/steam/l4d2z/srcds_run -game left4dead2 -port {port} +sv_clockcorrection_msecs 25 -timeout 10 -tickrate 100 +map c1m1_hotel -maxplayers 32 +servercfgfile server.cfg");
+        Script.Add($"sudo screen -d -m -S \"{port}\" /home/steam/l4d2z/srcds_run -game left4dead2 -port {port} +sv_clockcorrection_msecs 25 -timeout 10 -tickrate 100 +map c2m1_highway -maxplayers 32 +servercfgfile server.cfg");
     }
 }
