978e94e55fdac4660de805d314ab32b0e555ba44
Altair Sossai <altairsossai@gmail.com>
2022-12-01 13:54:43 -0300

    UserInfo AccessLevels

---
+++ L4D2ServerManager/Extensions/EnumExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace L4D2ServerManager.Extensions;
+
+public static class EnumExtensions
+{
+    public static IEnumerable<TEnum> Flags<TEnum>(this TEnum @enum)
+        where TEnum : Enum
+    {
+        return Enum.GetValues(typeof(TEnum)).Cast<TEnum>().Where(e => @enum.HasFlag(e));
+    }
+}
