dd39b77c15f677a166fc14f863c6663212b4ded3
Altair Sossai <altairsossai@gmail.com>
2022-12-13 18:55:18 -0300

    WIP

--- L4D2ServerManager.FunctionApp/Functions/SuspectedPlayerProcessFunction.cs
+++ L4D2ServerManager.FunctionApp/Functions/SuspectedPlayerProcessFunction.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using L4D2ServerManager.FunctionApp.Errors;
 using L4D2ServerManager.FunctionApp.Extensions;
@@ -43,7 +44,7 @@ public class SuspectedPlayerProcessFunction
 
 			var processes = _suspectedPlayerProcessRepository.GetAllProcesses(communityId);
 
-			return new OkObjectResult(processes);
+			return new OkObjectResult(processes.OrderBy(o => o.ProcessName).ToList());
 		}
 		catch (Exception exception)
 		{
